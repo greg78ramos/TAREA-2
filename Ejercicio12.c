dea7de17bf9d434ea52e85e6c30a666cbbce8e44
greg78ramos <gregoriomoralesramos17@gmail.com>
2026-05-01 02:29:17 -0600

    Create Ejercicio12.c

---
+++ Ejercicio12.c
@@ -0,0 +1,16 @@
+//TAREA 2 EJERCICIO 3 PROBLEMAS CON ESTRUCTURAS DE REPETICIÓN (FOR, WHILE)//
+
+#include <stdio.h>
+
+int main()
+{
+    int num, secuencia, producto=0, natural=0;
+    
+    printf("Calculemos el producto de los primeros numeros naturales de una secuencia con N números");
+    printf("Cuántos elementos contiene tu secuencia?");
+    scanf("%d", &secuencia);
+    
+    
+
+    return 0;
+}
