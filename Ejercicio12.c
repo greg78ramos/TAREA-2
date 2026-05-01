d836879416bcc8216fb410ea04a9196813c6fb0c
greg78ramos <gregoriomoralesramos17@gmail.com>
2026-05-01 12:00:31 -0600

    Update Ejercicio12.c

--- Ejercicio12.c
+++ Ejercicio12.c
@@ -4,13 +4,31 @@
 
 int main()
 {
-    int num, secuencia, producto=0, natural=0;
+    int num , i = 1 ;
+    long long producto = 1 ;
     
-    printf("Calculemos el producto de los primeros numeros naturales de una secuencia con N números");
-    printf("Cuántos elementos contiene tu secuencia?");
-    scanf("%d", &secuencia);
+    do //Validación de datos//
+    {
+        printf("Dame un numero entero, este puede encontrarse del 0 al infinito\n");
+        printf("Número: ");
+        scanf("%d", &num);
+    }
+    while( num < 0 );
     
-    
+    if( num == 0 ) //factorial de 0//
+    {
+        producto = 1;
+    }
+    else //factorial de cualquier número//
+    {
+        do
+        {
+            producto = producto * i ;
+            i = i + 1;
+        }
+    while( i <= num );
+    }
+    printf("%d! es igual a: %lld", num, producto);
 
     return 0;
 }
